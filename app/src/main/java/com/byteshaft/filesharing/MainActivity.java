b0a8711223026f239b2b276e42bfe257a11b8edd
rb-shahid <shgr8rb@gmail.com>
2017-01-17 15:22:13 +0500

    add activities

--- app/src/main/java/com/byteshaft/filesharing/MainActivity.java
+++ app/src/main/java/com/byteshaft/filesharing/MainActivity.java
@@ -2,12 +2,31 @@ package com.byteshaft.filesharing;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button buttonSend;
+    Button buttonRecevice;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        buttonSend = (Button) findViewById(R.id.button_send);
+        buttonRecevice = (Button) findViewById(R.id.button_receive);
+        buttonSend.setOnClickListener(this);
+        buttonRecevice.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.button_send:
+                break;
+            case R.id.button_receive:
+                break;
+        }
     }
 }
