6999a7ae36abe30761d07b741531dfe578dc2850
s9iper1 <s9iper1@gmail.com>
2017-01-23 22:13:07 +0500

    files and photos WIP

---
+++ app/src/main/java/com/byteshaft/filesharing/utils/Application.java
@@ -0,0 +1,15 @@
+package com.byteshaft.filesharing.utils;
+
+
+/**
+ * Created by s9iper1 on 1/23/17.
+ */
+
+public class Application extends android.app.Application {
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
